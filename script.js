8d3621eb8da84cde516b5afe4742561040e32a34
Dave Nash <davidjnash2@gmail.com>
2023-05-04 17:32:49 -0500

    updated javascript file to append each color block on click of each corresponding color button, and added a remove function to work for all blocks

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+$(document).ready(onReady); // first line load (boilerplate, it seems?)
 console.log('🟥 🟦 🟩 🟨');
+
+function onReady (){
+
+    $('#red-btn').on('click', appendRedBlock);
+
+    $('#blue-btn').on('click', appendBlueBlock);
+
+    $('#green-btn').on('click', appendGreenBlock);
+
+    $('#yellow-btn').on('click', appendYellowBlock);
+
+    $('#blocks').on('click', '.block', removeBlock);
+
+}
+
+
+
+
+function appendRedBlock() {
+    $('#blocks').append('<div class="block red-fill"></div>');
+}
+
+function appendBlueBlock() {
+    $('#blocks').append('<div class="block blue-fill"></div>');
+}
+
+function appendGreenBlock() {
+    $('#blocks').append('<div class="block green-fill"></div>');
+}
+
+function appendYellowBlock() {
+    $('#blocks').append('<div class="block yellow-fill"></div>');
+}
+
+function removeBlock() {
+    $(this).remove();
+}
